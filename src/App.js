496dcc0610a9ee81b0c45f8162f2a88b99015464
maxkram <max.kramer@live.com>
2022-06-21 17:04:58 -0400

    refactor: changed react to 17 version

--- src/App.js
+++ src/App.js
@@ -1,8 +1,95 @@
-import React from 'react';
-import './styles.css';
+import React, { useEffect } from "react";
+import Sidebar from "./components/Sidebar";
+import Editor from "./components/Editor";
+import { data } from "./data";
+import Split from "react-split";
+import { nanoid } from "nanoid";
+import "react-mde/lib/styles/css/react-mde-all.css";
+import "./App.css";
 
-function App() {
-    return <div> </div>;
+export default function App() {
+  const [notes, setNotes] = React.useState(
+    () => JSON.parse(localStorage.getItem("notes")) || []
+  );
+  const [currentNoteId, setCurrentNoteId] = React.useState(
+    (notes[0] && notes[0].id) || ""
+  );
+
+  function sortNotes(notes) {
+    setNotes(
+      notes.sort((a, b) => {
+        if (a.updatedAt < b.updatedAt) {
+          return 1;
+        }
+        if (a.updatedAt > b.updatedAt) {
+          return -1;
+        }
+        return 0;
+      })
+    );
+  }
+
+  useEffect(() => {
+    sortNotes(notes);
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes]);
+
+  function createNewNote() {
+    const newNote = {
+      id: nanoid(),
+      body: "# Type your markdown note's title here",
+      updatedAt: Date.now(),
+    };
+    setNotes((prevNotes) => [newNote, ...prevNotes]);
+    setCurrentNoteId(newNote.id);
+  }
+
+  function updateNote(text) {
+    setNotes((oldNotes) =>
+      oldNotes.map((oldNote) => {
+        return oldNote.id === currentNoteId
+          ? { ...oldNote, body: text, updatedAt: Date.now() }
+          : oldNote;
+      })
+    );
+  }
+
+  function deleteNote(event, noteId) {
+    event.stopPropagation();
+    setNotes((oldNotes) => oldNotes.filter((note) => note.id !== noteId));
+  }
+
+  function findCurrentNote() {
+    return (
+      notes.find((note) => {
+        return note.id === currentNoteId;
+      }) || notes[0]
+    );
+  }
+
+  return (
+    <main>
+      {notes.length > 0 ? (
+        <Split sizes={[30, 70]} direction="horizontal" className="split">
+          <Sidebar
+            notes={notes}
+            currentNote={findCurrentNote()}
+            setCurrentNoteId={setCurrentNoteId}
+            newNote={createNewNote}
+            deleteNote={deleteNote}
+          />
+          {currentNoteId && notes.length > 0 && (
+            <Editor currentNote={findCurrentNote()} updateNote={updateNote} />
+          )}
+        </Split>
+      ) : (
+        <div className="no-notes">
+          <h1>You have no notes</h1>
+          <button className="first-note" onClick={createNewNote}>
+            Create one now
+          </button>
+        </div>
+      )}
+    </main>
+  );
 }
-
-export default App;
